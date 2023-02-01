0e616a30f5ba81515e48edefc792827536bba056
Yous3f <yasilah4@gmail.com>
2023-02-01 11:34:44 +0200

    addedWhostart

--- Js/attack.js
+++ Js/attack.js
@@ -1,5 +1,21 @@
 
 
-function attackStart(){
-  
+function combatStart(){
+  checkFirstSart();
+//   monsterAttack();
+//   playerAttack();
+//   calcDamage();
+//   checkDefeat();
 }
+
+function checkFirstSart(player,monster){
+  rnd1=Math.floor(Math.random()*20)+1;
+  rnd2=Math.floor(Math.random()*20)+1;
+  pWIthDex=rnd1+player.dex;
+  MWIthDex=rnd2+monster.dex;
+  if(pWIthDex>MWIthDex){
+    return player;
+  }
+  return monster;
+} 
+
