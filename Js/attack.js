28fcafe13f1467f653cbb311d97b891122ad1d03
Yous3f <yasilah4@gmail.com>
2023-02-01 12:08:03 +0200

    fixingcheckfirstfunc

--- Js/attack.js
+++ Js/attack.js
@@ -1,23 +1,28 @@
+import {
+  monster1
+} from "./monster.js";
+import {
+  player1
+} from "./player.js";
 
-import {monster1} from "./monster.js";
-import {player1} from "./player.js";
+function combatStart() {
+  checkFirstStart(monster1, player1);
 
-function combatStart(){
-  checkFirstStart();
-//   monsterAttack();
-//   playerAttack();
-//   calcDamage();
-//   checkDefeat();
+  //   monsterAttack();
+  //   playerAttack();
+  //   calcDamage();
+  //   checkDefeat();
 }
 
-function checkFirstStart(monster1,player1){
-  rnd1=Math.floor(Math.random()*20)+1;
-  rnd2=Math.floor(Math.random()*20)+1;
-  pWIthDex=rnd1+player.dex;
-  MWIthDex=rnd2+monster.dex;
-  if(pWIthDex>MWIthDex){
+function checkFirstStart(monster, player) {
+  debugger
+  const rnd1 = Math.floor(Math.random() * 20) + 1;
+  const rnd2 = Math.floor(Math.random() * 20) + 1;
+  const pWIthDex = rnd1 + player.dexterity;
+  const MWIthDex = rnd2 + monster.dexterity;
+  if (pWIthDex > MWIthDex) {
     return player;
   }
   return monster;
-} 
+}
 
